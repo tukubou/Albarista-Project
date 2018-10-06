c0590135a8a35116bf51a547410306b75214a2e7
Yohei-Izumi <tamadan1385@gmail.com>
2018-10-06 16:55:33 +0900

    プルリクエストの指摘解消

--- autoCreateResearchSheet_gas.js
+++ autoCreateResearchSheet_gas.js
@@ -1,12 +1,11 @@
 function doPost(e) {
   var params = JSON.parse(e.postData.getDataAsString());
-  var table_index = 14; // ライバルセラーテーブルの先頭行
+  var tableIndex = 14; // ライバルセラーテーブルの先頭行
   
-  if(params.ranking > params.ranking_limit) {
+  if(params.ranking > params.rankingLimit) {
     // 対象ランキング圏外は無視
     return ;
   }
-
   // 親シートを取得
   var ss = SpreadsheetApp.openById(SpreadsheetApp.getActiveSpreadsheet().getId());
   var sheet = ss.getSheetByName("シート1");
@@ -14,40 +13,38 @@ function doPost(e) {
   var ssId = sheet.getRange('A2').getValues();
   var sheetId = sheet.getRange('B2').getValue();
   // 作成先シートを取得
-  var ss_copyTo = SpreadsheetApp.openById(ssId);
-  var sheet_copyTo = ss_copyTo.getSheetByName(sheetId);
-  if(ss_copyTo.getSheetByName(sheetId+" のコピー") == null) {
+  var ssCopyTo = SpreadsheetApp.openById(ssId);
+  var sheetCopyTo = ssCopyTo.getSheetByName(sheetId);
+  if(ssCopyTo.getSheetByName(sheetId+" のコピー") == null) {
     // バックアップを作っていない場合は作成
-    sheet_copyTo.copyTo(ss_copyTo);
+    sheetCopyTo.copyTo(ssCopyTo);
   }
-  var data_index = params.index;
+  var dataIndex = tableIndex;
   while(true) {
-    if(data_index >= table_index + 24) {
+    if(dataIndex >= tableIndex + 24) {
       // テーブルからはみ出ないように
       return;
-    }
-    else if(sheet_copyTo.getRange(data_index, 3).getValues() == "") {
+    } else if(sheetCopyTo.getRange(dataIndex, 3).getValues() == "") {
       // ブランドの項目を見て空白だったら入れる
       break;
     }
-    data_index += 1;
+    dataIndex += 1;
   }
   // データ入力
-  if(sheet_copyTo.getRange(3, 5).getValue() == "") {
+  if(sheetCopyTo.getRange('E3').getValue() == "") {
     // 商品名が空白の場合はカテゴリ、画像も合わせて設定する
-    sheet_copyTo.getRange(3, 5).setValue(sheetId.replace("【リサーチ】",""));
-    sheet_copyTo.getRange(4, 5).setValue(params.ranking);
-    sheet_copyTo.getRange(4, 2).setFormula("=IMAGE(\"" + params.image + "\")");
+    sheetCopyTo.getRange('E3').setValue(sheetId.replace("【リサーチ】",""));
+    sheetCopyTo.getRange('E4').setValue(params.category);
+    sheetCopyTo.getRange('B4').setFormula("=IMAGE(\"" + params.image + "\")");
   }
-  sheet_copyTo.getRange(data_index, 3).setValue(params.brand);
-  sheet_copyTo.getRange(data_index, 4).setValue(params.price);
-  sheet_copyTo.getRange(data_index, 5).setValue(params.category);
-  sheet_copyTo.getRange(data_index, 6).setValue(params.ranking);
-  sheet_copyTo.getRange(data_index, 13).setValue(params.review.good);
-  sheet_copyTo.getRange(data_index, 14).setValue(params.review.bad);
-  sheet_copyTo.getRange(data_index, 16).setValue(params.url);
-  sheet_copyTo.getRange(data_index, 17).setValue(" ");
+  sheetCopyTo.getRange(dataIndex, 3).setValue(params.brand);
+  sheetCopyTo.getRange(dataIndex, 4).setValue(params.price);
+  sheetCopyTo.getRange(dataIndex, 5).setValue(params.category);
+  sheetCopyTo.getRange(dataIndex, 6).setValue(params.ranking);
+  sheetCopyTo.getRange(dataIndex, 13).setValue(params.review.good);
+  sheetCopyTo.getRange(dataIndex, 14).setValue(params.review.bad);
+  sheetCopyTo.getRange(dataIndex, 16).setValue(params.url);
+  sheetCopyTo.getRange(dataIndex, 17).setValue(" ");
   
-
   return "success";
 }
--- autoCreateResearchSheet_monkey.js
+++ autoCreateResearchSheet_monkey.js
@@ -16,43 +16,39 @@
      if (href.includes("https://www.amazon.co.jp/s/")) {
          // 検索結果画面
         localStorage.clear();
-        const div_info_bar = /** @type {HTMLElement} */ (document.querySelector("[id=s-result-info-bar-content]"));
+        const divInfoBar = /** @type {HTMLElement} */ (document.querySelector("[id=s-result-info-bar-content]"));
         let bTag = document.createElement('button');
         bTag.textContent = "リサーチ管理表作成実行";
         bTag.onclick = function() {
             if(confirm("作成実行")) {
                 let index = 0;
                  bTag.textContent = "リサーチ管理表作成中";
-                const result_ul = /** @type {HTMLElement} */ (document.querySelector("[id=s-results-list-atf]"));
-                const result_li = result_ul.getElementsByClassName("s-result-item s-result-card-for-container-noborder s-carded-grid celwidget");
+                const ulResult = /** @type {HTMLElement} */ (document.querySelector("[id=s-results-list-atf]"));
+                const liResult = ulResult.getElementsByClassName("s-result-item s-result-card-for-container-noborder s-carded-grid celwidget");
                 const intervalId = setInterval(function() {
                      bTag.textContent = "リサーチ管理表作成中    :    " + (index+1) + "件目";
-                    if(result_li[index].getElementsByTagName("h5").length == 0) {
+                    if(liResult[index].getElementsByTagName("h5").length == 0) {
                         // スポンサープロダクトの表示がない場合
-                        const asin = result_li[index].getAttribute("data-asin");
+                        const asin = liResult[index].getAttribute("data-asin");
                         localStorage.setItem(index,asin);
-                        console.log(index);
-                        console.log(result_li[index]);
-                        const container = result_li[index].getElementsByTagName("div");
-                        const spacing_base = container[0].getElementsByClassName("a-spacing-base");
-                        const position_relative = spacing_base[0].getElementsByTagName("div");
-                        const a = position_relative[0].getElementsByTagName("a");
-                        a[0].click();
+                        const container = liResult[index].getElementsByTagName("div");
+                        const spacingBase = container[0].getElementsByClassName("a-spacing-base");
+                        const positionRelative = spacingBase[0].getElementsByTagName("div");
+                        const aTag = positionRelative[0].getElementsByTagName("a");
+                        aTag[0].click();
                     }
-                    console.log(index)
                     index++;
-                    if(index >= result_li.length) {
+                    if(index >= liResult.length) {
                         bTag.textContent = "リサーチ管理表作成中    完了";
                         clearInterval(intervalId);
                     }
-                }, 10000);
+                }, 6000);
             };
         }
-        div_info_bar.appendChild(bTag);
+        divInfoBar.appendChild(bTag);
     }else {
         // その他の画面
-        for(var j = 0; j < localStorage.length; j++ ) {
-            const key = localStorage.key(j)
+        for(const key in localStorage) {
             // 検索結果で保存したASINで遷移先を判断
             if(href.includes(localStorage.getItem(key))) {
                 // ランキング読み込みのためwaitは長め
@@ -65,37 +61,38 @@
         }
     }
     function getSelerData(key) {
-        const ranking_limit = 10000;
-        const div_organizations = /** @type {HTMLElement} */ (document.querySelector("[class='organizationInfo_description']"));
-        const a_brand = /** @type {HTMLElement} */ (document.querySelector("[id='bylineInfo']"));
-        const span_price = /** @type {HTMLElement} */ (document.querySelector("[id='priceblock_ourprice']"));
-        const div_enrich_container = /** @type {HTMLElement} */ (document.querySelector("[class='enrich-container']"));
-        const div_enrich_result = div_enrich_container.getElementsByTagName("div");
-        const span_enrich_result = div_enrich_result[0].getElementsByTagName("span");
-        const category = span_enrich_result[0].textContent;
-        const ranking = span_enrich_result[1].textContent.replace( /位/g , "" ).replace( /,/g , "" );
+        const rankingLimit = 3000;
+        const divOrganizations = /** @type {HTMLElement} */ (document.querySelector("[class='organizationInfo_description']"));
+        const aBrand = /** @type {HTMLElement} */ (document.querySelector("[id='bylineInfo']"));
+        const spanPrice = /** @type {HTMLElement} */ (document.querySelector("[id='priceblock_ourprice']"));
+        const divEnrichContainer = /** @type {HTMLElement} */ (document.querySelector("[class='enrich-container']"));
+        const divEnrichResult = divEnrichContainer.getElementsByTagName("div");
+        const spanEnrichResult = divEnrichResult[0].getElementsByTagName("span");
+        const category = spanEnrichResult[0].textContent;
+        const ranking = spanEnrichResult[1].textContent.replace( /位/g , "" ).replace( /,/g , "" );
         const image = /** @type {HTMLElement} */ (document.querySelector("[id='landingImage']"));
 
         // すべてのカスタマーレビューを表示するページに遷移するのが面倒なので全レビュー×割合でgood/badレビューを算出する
-        let span_totalReviewCount = /** @type {HTMLElement} */ (document.querySelector("[class='a-size-medium totalReviewCount']"));
-        span_totalReviewCount = (!span_totalReviewCount) ? 0 : Number(span_totalReviewCount.textContent) ;
-        var a_review_5star = /** @type {HTMLElement} */ (document.querySelector("[class='a-size-base a-link-normal 5star histogram-review-count']"));
-        a_review_5star = (!a_review_5star) ? 0 : Number(a_review_5star.textContent.slice( 0, -1 )) ;
-        var a_review_4star = /** @type {HTMLElement} */ (document.querySelector("[class='a-size-base a-link-normal 4star histogram-review-count']"));
-        a_review_4star = (!a_review_4star) ? 0 : Number(a_review_4star.textContent.slice( 0, -1 )) ;
-        var a_review_3star = /** @type {HTMLElement} */ (document.querySelector("[class='a-size-base a-link-normal 3star histogram-review-count']"));
-        a_review_3star = (!a_review_3star) ? 0 : Number(a_review_3star.textContent.slice( 0, -1 )) ;
-        var a_review_2star = /** @type {HTMLElement} */ (document.querySelector("[class='a-size-base a-link-normal 2star histogram-review-count']"));
-        a_review_2star = (!a_review_2star) ? 0 : Number(a_review_2star.textContent.slice( 0, -1 )) ;
-        var a_review_1star = /** @type {HTMLElement} */ (document.querySelector("[class='a-size-base a-link-normal 1star histogram-review-count']"));
-        a_review_1star = (!a_review_1star) ? 0 : Number(a_review_1star.textContent.slice( 0, -1 )) ;
-        const review_good = Math.round((a_review_4star + a_review_5star) * span_totalReviewCount / 100);
-        const review_bad = Math.round((a_review_1star + a_review_2star + a_review_3star) * span_totalReviewCount / 100);
+        let map = new Map([
+            ['totalReviewCount', 0],
+            ['1star', null], ['2star', null], ['3star', null],
+            ['4star', null], ['5star', null]
+        ]);
+        for (const [key, value] of map) {
+            if(key === 'totalReviewCount') {
+                let countRaw = /** @type {HTMLElement} */ (document.querySelector(`[class='a-size-medium ${key}']`) );
+                map.set(key,(!countRaw) ? 0 : Number(countRaw.textContent) );
+            } else {
+                let countRaw = /** @type {HTMLElement} */ (document.querySelector(`[class='a-size-base a-link-normal ${key} histogram-review-count']`) );
+                map.set(key, (!countRaw) ? 0 : Number(countRaw.textContent.slice( 0, -1 ) ) );
+            }
+        }
+        const reviewGood = Math.round((map.get('4star') + map.get('5star')) * map.get('totalReviewCount') / 100);
+        const reviewBad = Math.round(( map.get('1star') + map.get('2star') + map.get('3star')) * map.get('totalReviewCount') / 100);
 
         // windows だと ¥" でエスケープ　macだと \" でエスケープ
-        const params = "{\"index\":" + key + ",\"brand\":\"" + a_brand.textContent + "\",\"price\":" + Number(span_price.textContent.replace( /￥/g , "" ).replace( /,/g , "" ))
-        + ",\"category\":\"" + category + "\",\"ranking\":" + ranking + ",\"ranking_limit\":" + ranking_limit + ",\"url\":\""+href + "\",\"image\":\"" + image.src + "\",\"review\":{\"good\":" + review_good + ",\"bad\":" + review_bad + "}}"
-        console.log(params);
+        const params = `{\"index\": ${key} ,\"brand\":\"${aBrand.textContent}\",\"price\": ${ Number(spanPrice.textContent.replace( /￥/g , "" ).replace( /,/g , "" ))} ,
+        \"category\":\"${ category}\",\"ranking\": ${ranking} ,\"rankingLimit\": ${rankingLimit} ,\"url\":\"${href}\",\"image\":\"${image.src}\",\"review\":{\"good\": ${reviewGood} ,\"bad\": ${reviewBad} }}`;
         post(params);
     }
     function post(params) {
@@ -108,7 +105,6 @@
                 "Content-Type": "application/x-www-form-urlencoded",
             },
             onload: function(response) {
-                console.log("posted " + response);
             }
         });
     }
